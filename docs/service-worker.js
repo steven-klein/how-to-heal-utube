c8fe7af10fda9e4f81e15c8fd9702416cd422430
Steve Klein <steven.p.klein@gmail.com>
2019-07-31 17:20:52 -0500

    fresh build

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/how-to-heal-utube/precache-manifest.a4687c430bbf735c79a7a3cd88b54d75.js"
+  "/how-to-heal-utube/precache-manifest.450213113f007fa0240da84b0b425108.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-argon-dashboard"});
